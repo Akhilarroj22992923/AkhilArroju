c304b8d0f2c20f207042fd493849f8b7b9c33fb2
Akhilarroj22992923 <104203289+Akhilarroj22992923@users.noreply.github.com>
2022-04-22 18:42:22 +0530

    output added for typecasting
    we are listening wipro first class
    A = 74
    b = 74.0
    --------------------------------
    p = 4
    k = 
    --------------------------------
    t = 76456
    l = 76456
    c = -88

---
+++ typecasting.java
@@ -0,0 +1,31 @@
+package core.java.programs;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("we are listening wipro first class");
+		char status = 'J';
+		int A = (int)status;
+		float B = (float)A;
+		System.out.println("A = " + A);
+		System.out.println("b = " + B);
+		System.out.println("--------------------------------");
+		float h = 4.789f;
+		long p = (long)h;	
+		char k = (char)p;
+		System.out.println("p = " + p);
+		System.out.println("k = " + k);
+		System.out.println("--------------------------------");
+		double d = 76456.345678445;
+		int t = (int)d;
+		long l = (long)t;
+		byte o = (byte)l;
+		System.out.println("t = " + t);
+		System.out.println("l = " + l);
+		System.out.println("c = " + o);
+		
+		
+	}
+
+}
