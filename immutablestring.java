2291c900f5d26fb206c06fe0fea5d5dfd4938916
Akhilarroj22992923 <104203289+Akhilarroj22992923@users.noreply.github.com>
2022-04-24 17:47:57 +0530

    output is provided
    false....
    false....
    false....
    false....
    false....
    -------------------------------------------------------------------------------
    false....
    false....
    false....
    false....
    false....

---
+++ immutablestring.java
@@ -0,0 +1,91 @@
+package fourth_program;
+
+public class stringexamples {
+
+	public static void main(String[] args) {
+		
+		
+		 String t = "Delhi";  // String Literal---> objects are created in string pool
+		   String o = "Mumbai";
+		   String k = "delhi";
+		   String y = new String("Mumbai");
+		   String l = new String("delhi");
+		   String p = new String("Hello");
+		   
+		   if (o.equals(l))
+		   {
+			   System.out.println("true....");
+		   }
+		   else{
+			   System.out.println("false....");
+		   }
+		        
+		   
+		   if(y.equals(p))
+		   {
+			   System.out.println("true....");
+		   }
+		   else{
+			   System.out.println("false....");
+		   }
+		   if(t.equals(o))
+		   {
+			   System.out.println("true....");
+		   }
+		   else{
+			   System.out.println("false....");
+		   }
+		   if(k.equals(y))
+		   {
+			   System.out.println("true....");
+		   }
+		   else{
+			   System.out.println("false....");
+		   }
+		   if(p.equals(y))
+		   {
+			   System.out.println("true....");
+		   }
+		   else{
+			   System.out.println("false....");
+		   }
+		   System.out.println("-------------------------------------------------------------------------------");
+		   if (o == l){
+			   System.out.println("true....");
+		   }
+		   else{
+			   System.out.println("false....");
+			   
+		   }
+		   if (y == p){
+			   System.out.println("true....");
+		   }
+		   else{
+			   System.out.println("false....");
+			   
+		   }
+		   if (t == o){
+			   System.out.println("true....");
+		   }
+		   else{
+			   System.out.println("false....");
+			   
+		   }
+		   if (k == y){
+			   System.out.println("true....");
+		   }
+		   else{
+			   System.out.println("false....");
+			   
+		   }
+		   if (p == y){
+			   System.out.println("true....");
+		   }
+		   else{
+			   System.out.println("false....");
+			   
+		   }
+		   
+		  
+		 }  
+	 }
